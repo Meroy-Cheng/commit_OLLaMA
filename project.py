bfcdb785ba58d76278041a678910ca84fcb275a9
May <89690968+Meroy-Cheng@users.noreply.github.com>
2024-12-10 14:06:51 +0800

    Add files via upload

---
+++ project.py
@@ -0,0 +1,7 @@
+def generate_christmas_tree(height):
+    for i in range(height):
+        print(' ' * (height - i - 1) + '*' * (2 * i + 1))
+    print(' ' * (height - 1) + '|')
+
+tree_height = 6
+generate_christmas_tree(tree_height)
